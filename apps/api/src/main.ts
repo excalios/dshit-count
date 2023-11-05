54a1cc2fdd07fc3a477b54b6c2b82b9a059d8e1d
Ravi Deevan <ravideevan@duck.com>
2023-11-05 16:34:10 +0800

    first init

---
+++ apps/api/src/main.ts
@@ -0,0 +1,24 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import express from 'express';
+import cors from 'cors';
+import * as path from 'path';
+
+const app = express();
+
+app.use(cors());
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Welcome to api!' });
+});
+
+const port = process.env.PORT || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
--- apps/dshit-count/src/app/app.tsx
+++ apps/dshit-count/src/app/app.tsx
@@ -1,9 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import axios from 'axios';
 
 import NxWelcome from './nx-welcome';
 
-export function App() {
+export async function App() {
+  const res = await axios.get('http://localhost:3333/api');
+  // __AUTO_GENERATED_PRINT_VAR_START__
+  console.log('App res: ', res); // __AUTO_GENERATED_PRINT_VAR_END__
+
   return (
     <div>
       <NxWelcome title="dshit-count" />
